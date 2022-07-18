8c332061cc79c8ea3297075ca4fbf8a85807eddb
Joseph-Kirigo <joseph.kirigo@student.moringaschool.com>
2022-07-18 09:43:48 +0300

    add changes

--- script.js
+++ script.js
@@ -22,10 +22,9 @@ function totalProduction()
 
     document.getElementById("produce-body").innerHTML += "<tr><td>The total production is</td><td>" + total + " litres per day</td></tr>";
      
-     //reset a form after submit
+
     resetForm();
 
-     //calling incomeOverTime function and calculate income
     var daily = incomeOverTime(selling_price, 1);
     var weekly = incomeOverTime(selling_price, 7);
     var yearly = incomeOverTime(selling_price, 365);
@@ -36,7 +35,7 @@ function totalProduction()
     document.getElementById("produce-body").innerHTML += "<tr><td>Your Yearly income will be</td><td>Ksh " + (yearly*total) + "</td></tr>";
 
 
-       //  calculating leap year income
+    
     var leapyear = [
     { month : "January",income : (31 * total * selling_price)},
     { month : "February",income : (29 * total * selling_price) },
@@ -58,7 +57,6 @@ function totalProduction()
          document.getElementById("leapYear").innerHTML += "<tr><td>Your income for "+ leapyear[x].month + " is </td><td>Ksh. " + leapyear[x].income + "</td></tr></div>";
      }
 
-      // calling comparison function to compare monthly prices
     comparison(total)
 
 }
@@ -69,32 +67,32 @@ function incomeOverTime(selling_price, time)
 }
 
 
-function comparison(total)
-{
-     updated_price = 49.60;
-     var normalYear = [
-    { month : "January",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "February",income : Math.round(28 * total * updated_price).toFixed(2), difference : Math.round((29 * total * updated_price)-(28 * total * selling_price)).toFixed(2)  },
-    { month : "March",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "April",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
-    { month : "May",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "June",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
-    { month : "July",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "August",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "September",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
-    { month : "October",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
-    { month : "November",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
-    { month : "December",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) }
-    ];
+// function comparison(total)
+// {
+//      updated_price = 49.60;
+//      var normalYear = [
+//     { month : "January",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "February",income : Math.round(28 * total * updated_price).toFixed(2), difference : Math.round((29 * total * updated_price)-(28 * total * selling_price)).toFixed(2)  },
+//     { month : "March",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "April",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
+//     { month : "May",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "June",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
+//     { month : "July",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "August",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "September",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
+//     { month : "October",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) },
+//     { month : "November",income : Math.round(30 * total * updated_price).toFixed(2), difference : Math.round((30 * total * updated_price)-(30 * total * selling_price)).toFixed(2) },
+//     { month : "December",income : Math.round(31 * total * updated_price).toFixed(2), difference : Math.round((31 * total * updated_price)-(31 * total * selling_price)).toFixed(2) }
+//     ];
     
-    document.getElementById("caption-div").innerHTML += '<div class="alert text-white bg-dark h4">Earnings after price increase and difference</div> ';
-    document.getElementById("comparison-headers").innerHTML += "<tr><td>Month</td><td>Earnings(Ksh) @49/-</td><td>difference from 45/-(Ksh)</td></tr>";
+//     document.getElementById("caption-div").innerHTML += '<div class="alert text-white bg-dark h4">Earnings after price increase and difference</div> ';
+//     document.getElementById("comparison-headers").innerHTML += "<tr><td>Month</td><td>Earnings(Ksh) @49/-</td><td>difference from 45/-(Ksh)</td></tr>";
     
-    for(var x=0;x<12;x++)
-     {
-         document.getElementById("comparison").innerHTML += "<tr><td>"+ normalYear[x].month + "</td><td>" + normalYear[x].income + "</td><td>" + normalYear[x].difference +  "</td></tr>";
-     }
-}
+//     for(var x=0;x<12;x++)
+//      {
+//          document.getElementById("comparison").innerHTML += "<tr><td>"+ normalYear[x].month + "</td><td>" + normalYear[x].income + "</td><td>" + normalYear[x].difference +  "</td></tr>";
+//      }
+// }
 
 function resetForm()
 {
